e4be6e717e0ee2e3b515d9be01f9a9b2a3c13746
Nelson Ochoa <nelsonochoam@gmail.com>
2023-02-13 17:10:40 -0700

    feat: template for using testing framework with HelloWorld component

---
+++ templates/java/TestingFramework/component/src/main/java/com/templates/otf/App.java
@@ -0,0 +1,14 @@
+package com.templates.otf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args)
+    {
+        //  ------ Start writing your component logic here --------
+        System.out.println( "Hello World!" );
+    }
+}
