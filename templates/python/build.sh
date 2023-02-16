c0a82e95df05fa58573816afd698c453eb20ac7e
Nelson Ochoa <ochoanel@amazon.com>
2023-02-16 08:09:55 -0700

    fix: call python templates variable

--- templates/python/build.sh
+++ templates/python/build.sh
@@ -2,7 +2,7 @@
 
 python_templates=$(ls -d */ | sed 's/\//\n/g')
 current_directory=`pwd`
-for template in $maven_templates; do
+for template in $python_templates; do
    cd $current_directory/$template
    echo "Building $template template"
 done
